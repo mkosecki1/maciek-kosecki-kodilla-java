b33884577b7c255d8d1842d7837ff43ce64388cb
“mkosecki1 <mkosecki@op.pl>
2017-10-10 18:53:11 +0200

    Secend commit

--- gradlew.bat
+++ gradlew.bat
@@ -1,4 +1,4 @@
-@if "%DEBUG%" == "" @echo off
+c@if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
