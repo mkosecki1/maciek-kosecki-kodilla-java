35ca32499e5696e1b0f8b21db89d8195a3c7ec81
“mkosecki1 <mkosecki@op.pl>
2017-10-10 18:55:56 +0200

    Secend commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -3,7 +3,7 @@ package com.kodilla.testing;
 public class TestingMain {
 
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania!");
 
     }
 }
