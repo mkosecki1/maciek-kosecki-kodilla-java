6f7433770056e90f0d2680824f3a13e4e470ec21
“mkosecki1 <mkosecki@op.pl>
2017-10-14 18:40:41 +0200

    Zadanie 6.3. - poprawione

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -5,15 +5,16 @@ import java.util.ArrayList;
 class OddNumbersExterminator {
 
     public ArrayList<Integer> exterminate(ArrayList<Integer> numbers){
-        int tempValue = 0;
+        int tempValue;
+        ArrayList<Integer> output = new ArrayList<>();
         for (int i=0; i<numbers.size(); i++){
             tempValue = numbers.get(i);
             if (tempValue % 2 == 0){
-                System.out.println(tempValue);
+                output.add(tempValue);
             }
         }
 
-        return numbers;
+        return output;
     }
 
     public static void main(String[] args) {
