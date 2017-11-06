53088748857176c80e0f7f96210935f16f7d4f27
“mkosecki1 <mkosecki@op.pl>
2017-11-06 17:49:33 +0100

    9.5. update2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flights/ApplicationFlights.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flights/ApplicationFlights.java
@@ -1,10 +1,18 @@
 package com.kodilla.good.patterns.challenges.flights;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class ApplicationFlights {
     public static void main(String[] args){
         FlightProcessor flightProcessor = new FlightProcessor();
-        System.out.println(flightProcessor.departureSearcher("Wrocław"));
+        List<Set<Flights>> x = new ArrayList<>();
+        x.add(flightProcessor.departureSearcher("Wrocław"));
+        for (Set<Flights> xxx:x) {
+            System.out.println(xxx);
+        }
         System.out.println(flightProcessor.arrivalSearcher("Wrocław"));
-        System.out.println(flightProcessor.transferSearcher("MM"));
+        System.out.println(flightProcessor.transferSearcher("Kraków"));
     }
 }
