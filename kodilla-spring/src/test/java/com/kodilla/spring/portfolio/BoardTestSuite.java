ec42e3da2f84d93c2bec34770c2f90374d941cf5
“mkosecki1 <mkosecki@op.pl>
2017-11-14 17:38:29 +0100

    10.4.

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -6,7 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class BoardTestSuite {
-    @Test
+
     public void testTaskAdd(){
         //Given
         ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
