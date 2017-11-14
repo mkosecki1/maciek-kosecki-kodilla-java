1b4bc1c3d39de16bc9c47e3e01c7ee68c79eb9cc
“mkosecki1 <mkosecki@op.pl>
2017-11-14 19:34:52 +0100

    11.1

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,24 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+    private static Logger logger;
+
+    @BeforeClass
+    public static void openLoggerFile(){
+        Logger.getInstance().log("This is my first log");
+    }
+
+    @Test
+    public void getLastLogTest(){
+        //Given
+        //When
+        String lastLog = Logger.getInstance().getLastLog();
+        System.out.println("Log text: " + lastLog);
+        //Then
+        Assert.assertEquals("This is my first log", lastLog);
+    }
+}
