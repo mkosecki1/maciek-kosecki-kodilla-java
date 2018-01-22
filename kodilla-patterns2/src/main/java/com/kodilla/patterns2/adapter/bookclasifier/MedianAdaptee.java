b56a6f3345a4e42e037d98f17c136f02138fbf56
“mkosecki1 <mkosecki@op.pl>
2018-01-22 19:33:13 +0100

    exercise 20.5.

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdaptee.java
@@ -0,0 +1,22 @@
+package com.kodilla.patterns2.adapter.bookclasifier;
+
+import com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book;
+import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSignature;
+import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookStatistics;
+import com.kodilla.patterns2.adapter.bookclasifier.libraryb.Statistics;
+
+import java.util.Map;
+
+public class MedianAdaptee implements BookStatistics {
+    @Override
+    public int averagePublicationYaer(Map<BookSignature, Book> books) {
+        Statistics theStatistics = new Statistics();
+        return theStatistics.averagePublicationYaer(books);
+    }
+
+    @Override
+    public int medianPublicationYaer(Map<BookSignature, Book> books) {
+        Statistics theStatistics = new Statistics();
+        return theStatistics.medianPublicationYaer(books);
+    }
+}
---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
@@ -0,0 +1,24 @@
+package com.kodilla.patterns2.adapter.bookclasifier;
+
+import com.kodilla.patterns2.adapter.bookclasifier.librarya.Book;
+import com.kodilla.patterns2.adapter.bookclasifier.librarya.Classifier;
+import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSignature;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MedianAdapter extends MedianAdaptee implements Classifier {
+    @Override
+    public int publicationyearYearMedian(Set<Book> bookSet) {
+        Map<BookSignature, com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book> books = new HashMap<>();
+        for (Book thatBook : bookSet){
+            books.put(new BookSignature(thatBook.getSignature()),
+                    new com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book(
+                            thatBook.getAuthor(),
+                            thatBook.getTitle(),
+                            thatBook.getPublicationYear()));
+        }
+        return medianPublicationYaer(books);
+    }
+}
