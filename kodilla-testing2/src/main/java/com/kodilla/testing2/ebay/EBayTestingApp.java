cae67176def0645957da070c317c50b9defea9d3
“mkosecki1 <mkosecki@op.pl>
2018-02-09 18:12:36 +0100

    zadanie_23_2

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -0,0 +1,17 @@
+package com.kodilla.testing2.ebay;
+
+import com.kodilla.testing2.google.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class EBayTestingApp {
+    public static void main(String[] args){
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
+        driver.get("https://www.ebay.com/");
+
+        WebElement searchElement = driver.findElement(By.name("_nkw"));
+        searchElement.sendKeys("Laptop");
+        searchElement.submit();
+    }
+}
---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/google/GoogleTestingApp.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing2.google.google;
+
+import com.kodilla.testing2.google.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GoogleTestingApp {
+    public static final String SEARCHFIELD = "lst-ib";
+
+    public  static void main(String[] args){
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
+        driver.get("https://www.google.pl");
+
+        WebElement searchField = driver.findElement(By.id(SEARCHFIELD));
+        searchField.sendKeys("Kodilla");
+        searchField.submit();
+    }
+}
